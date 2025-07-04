a2fe00c09ccd493ec174cf29cff33fe71c0de26e
Alessandro Pispola <134197059+AlePispola@users.noreply.github.com>
2025-07-04 14:44:46 +0200

    Update YamlCreation.sh

--- Presentation/Istio/YamlCreation.sh
+++ Presentation/Istio/YamlCreation.sh
@@ -190,4 +190,4 @@ kubectl apply -f exam-scheduler.yaml
 kubectl apply -f professor-tools.yaml
 kubectl apply -f student-portal.yaml
 
-echo "✅ All YAML files created successfully."
+echo "✅ All YAML files applied successfully."
