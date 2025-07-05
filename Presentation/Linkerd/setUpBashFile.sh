214a111116f1863674db755f8ef9a9bcc7062eee
Simone Capriolo <112384282+Simolaaaab@users.noreply.github.com>
2025-07-05 10:31:27 +0200

    Create setUpBashFile.sh

---
+++ Presentation/Linkerd/setUpBashFile.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+chmod +x SetErrorAura0.9.sh
+chmod +x LinkerdSetup.sh
+chmod +x YamlCreation.sh
+chmod +x checkEjectionChaoPod.sh
+chmod +x checkRPS.sh
+chmod +x checkStatus.sh
+chmod +x routeAura.sh
+chmod +x routeChaosExB.sh
